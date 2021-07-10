570d394d864b148565960bd13b1dd2b1494bbce8
Blvisse <blaisepke@gmail.com>
2021-07-10 13:41:53 +0300

    fixed heroku url

--- request.py
+++ request.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = 'https://git.heroku.com/loan-prediction-pp.git'
+url = 'https://simple-loanpredictor.herokuapp.com/'
 
 
 data= {'Gender':1,'Married':1,'Dependents':3,'Education':1,'Self_Employed':0,'ApplicantIncome':340234,'CoapplicantIncome':280456,'LoanAmount':234000,'Loan_Amount_Term':24,'Credit_History':1,'Property_Area':1}
